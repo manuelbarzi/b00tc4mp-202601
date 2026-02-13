a348dd0013ede72a2fa6fed5c65546b6dd4e8b18
manuelbarzi <manuelbarzi@gmail.com>
2026-02-13 19:01:39 +0100

    add array-n-objects teams challenge

---
+++ staff/manuel-barzi/playground/js/arrays-n-objects/teams.js
@@ -0,0 +1,25 @@
+// teams
+
+var teams = []
+
+teams[0] = {}
+teams[0]['Chicago Bulls'] = []
+teams[0]['Chicago Bulls'][0] = {}
+teams[0]['Chicago Bulls'][0]['Michael Jordan'] = []
+
+// TODO add more teams (3) and players (3 per team)
+// TODO print the players in the console (showing the team, the players, and the info related to them)
+/*
+[Team name]
+- [Player name] ([jersey], [height]cm)
+- [Player name] ([jersey], [height]cm)
+...
+[Team name]
+- [Player name] ([jersey], [height]cm)
+- [Player name] ([jersey], [height]cm)
+...
+[Team name]
+- [Player name] ([jersey], [height]cm)
+- [Player name] ([jersey], [height]cm)
+...
+*/
