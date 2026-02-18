7272a8d03c3d37010e7614a01469ac5a7cdc48c4
manuelbarzi <manuelbarzi@gmail.com>
2026-02-18 09:55:35 +0100

    add teams-arrays-objects challenge

--- staff/manuel-barzi/playground/js/teams-arrays-objects.js
+++ staff/manuel-barzi/playground/js/teams-arrays-objects.js
@@ -3,9 +3,12 @@
 var teams = []
 
 teams[0] = {}
-teams[0]['Chicago Bulls'] = []
-teams[0]['Chicago Bulls'][0] = {}
-teams[0]['Chicago Bulls'][0]['Michael Jordan'] = []
+teams[0]['name'] = 'Chicago Bulls'
+teams[0]['players'] = []
+teams[0]['players'][0] = {}
+teams[0]['players'][0]['name'] = 'Michael Jordan'
+teams[0]['players'][0]['jersey'] = 23
+teams[0]['players'][0]['height'] = 200
 
 // TODO add more teams (3) and players (3 per team)
 // TODO print the players in the console (showing the team, the players, and the info related to them)
