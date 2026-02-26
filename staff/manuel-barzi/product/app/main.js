962ee3a7563053408bd05b350b8ef9bbe57b1b86
manuelbarzi <manuelbarzi@gmail.com>
2026-02-26 18:26:50 +0100

    adjust app initial code

--- staff/manuel-barzi/product/app/main.js
+++ staff/manuel-barzi/product/app/main.js
@@ -1,18 +1,22 @@
+var titleText = document.createTextNode('App')
+document.head.children[2].appendChild(titleText)
+
 var landingView = document.createElement('div')
 
 var landingHeader = document.createElement('header')
-var landingLogoImage = document.createElement('img')
-landingLogoImage.src = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTUcqp7uem68vwBG25scxHy0PPoFJbNVFsdAfQsDqsBwg&s'
-landingLogoImage.style.width = '50px'
-landingHeader.appendChild(landingLogoImage)
+var landingLogo = document.createElement('img')
+landingLogo.src = 'https://cdn.prod.website-files.com/624ac40503a527cf47af4192/659ba59520d886f0cb86d3ba_ai-logo-generator-4.png'
+landingLogo.style.width = '100px'
+landingHeader.appendChild(landingLogo)
 var landingHeaderSpaceText = document.createTextNode(' ')
 landingHeader.appendChild(landingHeaderSpaceText)
 var landingTitle = document.createElement('h1')
-landingTitle.style.display = 'inline-block'
+landingTitle.style.display = 'inLine-block' 
 var landingTitleText = document.createTextNode('App')
 landingTitle.appendChild(landingTitleText)
 landingHeader.appendChild(landingTitle)
 landingView.appendChild(landingHeader)
+
 
 var landingParagraph = document.createElement('p')
 var landingLoginLink = document.createElement('a')
@@ -29,21 +33,19 @@ landingRegisterLink.appendChild(landingRegisterText)
 landingParagraph.appendChild(landingRegisterLink)
 landingView.appendChild(landingParagraph)
 
-// document.body.appendChild(landingView)
-
-// TODO implement login view
+document.body.appendChild(landingView)
 
 var loginView = document.createElement('div')
 
 var loginHeader = document.createElement('header')
-var loginLogoImage = document.createElement('img')
-loginLogoImage.src = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTUcqp7uem68vwBG25scxHy0PPoFJbNVFsdAfQsDqsBwg&s'
-loginLogoImage.style.width = '50px'
-loginHeader.appendChild(loginLogoImage)
+var loginLogo = document.createElement('img')
+loginLogo.src = 'https://cdn.prod.website-files.com/624ac40503a527cf47af4192/659ba59520d886f0cb86d3ba_ai-logo-generator-4.png'
+loginLogo.style.width = '100px'
+loginHeader.appendChild(loginLogo)
 var loginHeaderSpaceText = document.createTextNode(' ')
 loginHeader.appendChild(loginHeaderSpaceText)
 var loginTitle = document.createElement('h1')
-loginTitle.style.display = 'inline-block'
+loginTitle.style.display = 'inLine-block' 
 var loginTitleText = document.createTextNode('App')
 loginTitle.appendChild(loginTitleText)
 loginHeader.appendChild(loginTitle)
@@ -75,11 +77,85 @@ loginView.appendChild(loginForm)
 
 var loginRegisterLink = document.createElement('a')
 loginRegisterLink.href = ''
-var loginRegisterText = document.createTextNode('Register')
-loginRegisterLink.appendChild(loginRegisterText)
+var logingRegisterText = document.createTextNode('Register')
+loginRegisterLink.appendChild(logingRegisterText)
 loginView.appendChild(loginRegisterLink)
 
 document.body.appendChild(loginView)
 
-// TODO implement register view (name, email, username, password)
-// TODO implement home view (a title with "Hello, Home!")
+var registerView = document.createElement('div')
+
+var registerHeader = document.createElement('header')
+var registerLogo = document.createElement('img')
+registerLogo.src = 'https://cdn.prod.website-files.com/624ac40503a527cf47af4192/659ba59520d886f0cb86d3ba_ai-logo-generator-4.png'
+registerLogo.style.width = '100px'
+registerHeader.appendChild(registerLogo)
+var registerHeaderSpaceText = document.createTextNode(' ')
+registerHeader.appendChild(registerHeaderSpaceText)
+var registerTitle = document.createElement('h1')
+registerTitle.style.display = 'inLine-block' 
+var registerTitleText = document.createTextNode('App')
+registerTitle.appendChild(registerTitleText)
+registerHeader.appendChild(registerTitle)
+registerView.appendChild(registerHeader)
+
+var registerTitle = document.createElement('h2')
+var registerTitleText = document.createTextNode('Register')
+registerTitle.appendChild(registerTitleText)
+registerView.appendChild(registerTitle)
+
+var registerForm = document.createElement('form')
+var registerNameLabel = document.createElement('label')
+var registerNameLabelText = document.createTextNode('Name')
+registerNameLabel.appendChild(registerNameLabelText)
+registerForm.appendChild(registerNameLabel)
+var registerNameInput = document.createElement('input')
+registerForm.appendChild(registerNameInput)
+var registerEmailLabel = document.createElement('label')
+var registerEmailLabelText = document.createTextNode('Email')
+registerEmailLabel.appendChild(registerEmailLabelText)
+registerForm.appendChild(registerEmailLabel)
+var registerEmailInput = document.createElement('input')
+registerForm.appendChild(registerEmailInput)
+var registerUsernameLabel = document.createElement('label')
+var registerUsernameLabelText = document.createTextNode('Username')
+registerUsernameLabel.appendChild(registerUsernameLabelText)
+registerForm.appendChild(registerUsernameLabel)
+var registerUsernameInput = document.createElement('input')
+registerForm.appendChild(registerUsernameInput)
+var registerPasswordLabel = document.createElement('label')
+var registerPasswordLabelText = document.createTextNode('Password')
+registerPasswordLabel.appendChild(registerPasswordLabelText)
+registerForm.appendChild(registerPasswordLabel)
+var registerPasswordInput = document.createElement('input')
+registerForm.appendChild(registerPasswordInput)
+registerView.appendChild(registerForm)
+var registerSubmitButton = document.createElement('button')
+var registerSubmitButtonText = document.createTextNode('Register')
+registerSubmitButton.appendChild(registerSubmitButtonText)
+registerForm.appendChild(registerSubmitButton)
+
+document.body.appendChild(registerView)
+
+var homeView = document.createElement('div')
+
+var homeHeader = document.createElement('header')
+var homeLogo = document.createElement('img')
+homeLogo.src = 'https://cdn.prod.website-files.com/624ac40503a527cf47af4192/659ba59520d886f0cb86d3ba_ai-logo-generator-4.png'
+homeLogo.style.width = '100px'
+homeHeader.appendChild(homeLogo)
+var homeHeaderSpaceText = document.createTextNode(' ')
+homeHeader.appendChild(homeHeaderSpaceText)
+var homeTitle = document.createElement('h1')
+homeTitle.style.display = 'inLine-block' 
+var homeTitleText = document.createTextNode('App')
+homeTitle.appendChild(homeTitleText)
+homeHeader.appendChild(homeTitle)
+homeView.appendChild(homeHeader)
+
+var homeTitle = document.createElement('h2')
+var homeTitleText = document.createTextNode('Hello, Home!')
+homeTitle.appendChild(homeTitleText)
+homeView.appendChild(homeTitle)
+
+document.body.appendChild(homeView)
